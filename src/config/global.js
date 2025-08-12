cc36146a542c375b548e6ed5a3ce0528e107f1b5
jhonuru <47787645+jhonuru@users.noreply.github.com>
2025-08-12 14:31:17 -0500

    Ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -256,7 +256,7 @@ export default {
       ],
     },
     {
-      titulo: 'GESTORES DE REPOSITORIO',
+      titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
           nombre: 'Aixa Natalia Sendoya Fernández',
